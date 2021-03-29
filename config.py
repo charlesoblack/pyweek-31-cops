9434bcd63c17c90a16e9fa378c17271595e45634
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2021-03-29 02:59:45 -0400

    blit old blocks

--- config.py
+++ config.py
@@ -6,7 +6,21 @@ fontname = 'Arial'
 fontsize = 32
 
 level_colors = [(255, 0, 0),  # red
+                (255, 0, 255),  # magenta
                 (0, 255, 0),  # green
                 (0, 0, 255),  # blue
-                (255, 0, 255),  # magenta
+                (0, 255, 255),  # cyan?
+                (255, 255, 0),  # yellow?
                 ]
+
+num_levels = len(level_colors) - 2
+
+raw_origins = [(width // 2, 0),
+               (width // 2, height),
+               (0, height // 2),
+               (width, height // 2),
+               (0, height),
+               (width, height),
+               ]
+
+origins = {k: v for k, v in zip(level_colors, raw_origins)}
--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import time
 class COPS(object):
 
     def __init__(self):
-        self.surface = pygame.display.set_mode((config.width, config.height))
+        self.width, self.height = config.width, config.height
+        self.surface = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption('COPS: Cops Organize Paper Stacks')
 
         self.font = pygame.font.SysFont(config.fontname, config.fontsize)
@@ -22,15 +23,16 @@ class COPS(object):
         self.run()
 
     def start_game(self):
-        self.level = 0
-        self.start_timer = 3
+        self.level = 1
+        self.start_timer = 10
         self.points = 0
-        self.base_surface = pygame.Surface((config.width, config.height))
+        self.base_surface = pygame.Surface((self.width, self.height))
+        self.old_blocks = []
         self.blit_background()
 
     def blit_background(self):
         # TODO
-        background = pygame.Surface((config.width, config.height))
+        background = pygame.Surface((self.width, self.height))
         # blit background based on level?
         self.base_surface.blit(background, (0, 0))
 
@@ -42,16 +44,14 @@ class COPS(object):
 
     @property
     def current_colors(self):
-        return config.level_colors[: self.level + 3]
+        return config.level_colors[: self.level + 2]
+
+    @property
+    def game_over(self):
+        return self.level > config.num_levels
 
     def place_random_block(self):
         self.right_color = random.choice(self.current_colors)
-
-        block = pygame.Surface((50, 50))
-        block_location = ((config.width - 50) // 2, (config.height - 50) // 2)
-        block.fill(pygame.Color(*self.right_color))
-
-        self.base_surface.blit(block, block_location)
 
     def blit_text(self, text, **location):
         render = self.font.render(text, True, self.font_color)
@@ -61,17 +61,46 @@ class COPS(object):
     def blit_infos(self):
         self.blit_text(f'Level: {self.level}', topleft=(10, 5))
         self.blit_text(f'Time left: {self.time_left:02.0f}',
-                       topright=(config.width - 10, 5))
+                       topright=(self.width - 10, 5))
         self.blit_text(f'Points: {self.points:03.0f}',
-                       midtop=(config.width // 2, 5))
+                       midtop=(self.width // 2, 5))
 
     def blit_final_score(self):
         self.blit_text(f'Final tally: {self.points:03.0f}',
-                       center=(config.width // 2,
-                               config.height // 2))
+                       center=(self.width // 2,
+                               self.height // 2))
         self.blit_text(f'Press R to restart',
-                       center=(config.width // 2,
-                               config.height // 2 + 40))
+                       center=(self.width // 2,
+                               self.height // 2 + 40))
+
+    def blit_block(self, color, **location):
+        block = pygame.Surface((50, 50))
+        block_location = block.get_rect(**location)
+        block.fill(pygame.Color(*color))
+
+        self.surface.blit(block, block_location)
+
+    def blit_current_block(self):
+        self.blit_block(self.right_color,
+                        center=(self.width // 2, self.height // 2))
+
+    @staticmethod
+    def interpolate(origin, percent):
+        center_width = ((100 - percent) * config.width // 200
+                        + percent * origin[0] // 100)
+        center_height = ((100 - percent) * config.height // 200
+                         + percent * origin[1] // 100)
+        return (center_width, center_height)
+
+    def blit_old_blocks(self):
+        for (color, percent) in self.old_blocks:
+            if percent >= 100:
+                continue
+            # implicit else
+            origin = config.origins[color]
+            self.blit_block(color, center=self.interpolate(origin, percent))
+        self.old_blocks = [(color, percent + 1)
+                           for color, percent in self.old_blocks]
 
     def run(self):
 
@@ -79,6 +108,8 @@ class COPS(object):
                        pygame.K_DOWN: (255, 0, 255),
                        pygame.K_LEFT: (0, 255, 0),
                        pygame.K_RIGHT: (0, 0, 255),
+                       pygame.K_a: (0, 255, 255),
+                       pygame.K_s: (255, 255, 0),
                        pygame.K_q: 'quit',
                        pygame.K_r: 'restart',
                        }
@@ -97,12 +128,14 @@ class COPS(object):
                 self.level += 1
                 start_time = time.time()
 
-            if self.level > 1:
+            if self.game_over:
                 self.reset_surface()
                 self.blit_final_score()
             else:
                 self.reset_to_base_surface()
                 self.blit_infos()
+                self.blit_current_block()
+                self.blit_old_blocks()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -115,7 +148,7 @@ class COPS(object):
                         pygame.quit()
                         sys.exit()
 
-                    if self.level > 1:
+                    if self.game_over:
                         if color == 'restart':
                             self.start_game()
                             self.place_random_block()
@@ -125,6 +158,7 @@ class COPS(object):
 
                         if self.right_color == color:
                             self.points += 1
+                            self.old_blocks.append((color, 0))
                             self.place_random_block()
 
             pygame.display.flip()
