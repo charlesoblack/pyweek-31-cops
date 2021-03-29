0649b561ede23708c6e8da8cffab097a9df2f50b
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2021-03-29 00:32:02 -0400

    add level colors

--- config.py
+++ config.py
@@ -4,3 +4,9 @@ width = 800
 height = 600
 fontname = 'Arial'
 fontsize = 32
+
+level_colors = [(255, 0, 0),  # red
+                (0, 255, 0),  # green
+                (0, 0, 255),  # blue
+                (255, 0, 255),  # magenta
+                ]
